21522e847c7cfd32f963ad53623d611977000b26
Chen Xiao <chenxiao770117@gmail.com>
2024-05-24 00:02:17 +0800

    update

--- fast-xml/main.rkt
+++ fast-xml/main.rkt
@@ -5,8 +5,18 @@
           [lists-to-xml (-> list? string?)]
           ))
 
+(define (def->hash def)
+  (let ([def_hash (make-hash)])
+    (let loop-def ([loop_defs def])
+      (when (not (null? loop_defs))
+        (hash-set! def_hash (caar loop_defs) (cdar loop_defs))
+        (loop-def (cdr loop_defs))))
+    def_hash))
+
 (define (xml-defs-to-hash in def)
-  (make-hash))
+  (let ([xml_hash (make-hash)]
+        [def_hash (def->hash def)])
+    xml_hash))
 
 (define (lists-to-xml xml_list)
   "")
