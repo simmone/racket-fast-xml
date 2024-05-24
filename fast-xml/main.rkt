212997f77fa1ca98a97889e1680f0ca4856c72da
Chen Xiao <chenxiao770117@gmail.com>
2024-05-24 17:18:46 +0800

    update

--- fast-xml/main.rkt
+++ fast-xml/main.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (provide (contract-out
-          [xml-defs-to-hash (-> (or/c path-string? input-port?) list? hash?)]
+          [xml-file-to-hash (-> path-string? list? hash?)]
+          [xml-port-to-hash (-> input-port? list? hash?)]
           [lists-to-xml (-> list? string?)]
           ))
 
@@ -13,9 +14,31 @@
         (loop-def (cdr loop_defs))))
     def_hash))
 
-(define (xml-defs-to-hash in def)
+(define (xml-file-to-hash xml_file def)
+  (with-input-from-file xml_file
+    (lambda ()
+      (xml-port-to-hash (current-input-port) def))))
+
+; 'START: scan start
+; 'WAITING_KEY: waiting to encounter '<'
+; 'READING_KEY: next chars will be read as key
+
+(define (xml-port-to-hash xml_port def)
   (let ([xml_hash (make-hash)]
         [def_hash (def->hash def)])
+    (let loop ([ch (read-char xml_port)]
+               [status 'START]
+               [value ''])
+      (when (not (eof-object? ch))
+        (cond
+         [(eq? status 'START)
+          (loop ch 'WAITING_KEY value)]
+         [(eq? status 'WAITING_KEY)
+          (if (char=? ch #\<)
+              (loop (read-char) 'READING_KEY value)
+              (loop (read-char) 'WAITING_KEY value))]
+         [else
+          (void)])))
     xml_hash))
 
 (define (lists-to-xml xml_list)
