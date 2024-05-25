894ba8cdcb3788bcc2f85905f6ca9a652ae138a2
Chen Xiao <chenxiao770117@gmail.com>
2024-05-25 15:13:36 +0800

    update

--- fast-xml/main.rkt
+++ fast-xml/main.rkt
@@ -9,19 +9,19 @@
 (define (xml-file-to-hash xml_file def_pairs)
   (with-input-from-file xml_file
     (lambda ()
-      (xml-port-to-hash (current-input-port) def))))
+      (xml-port-to-hash (current-input-port) def_pairs))))
 
 ; loop parameters:
 ; ch: read a char each loop.
 ; defs: searching defines, '(key1 (key2 (key3 . 'v)))
 ; status: current search status.
 ; keys: combine find keys to a list: '(key1 key2 key3...)
-; value: combine a string from chars
+; value: combine char to a list.
 
 ; 'START: scan start
 ; 'KEY_START: waiting to encounter '<'
 ; 'READING_OR_KEY_END: next chars will be read as key, or waiting to encounter '>'
-; 'VALUE_OR_KEY_START: reading value
+; 'VALUE: reading value
 
 (define (xml-port-to-hash xml_port def_pairs)
   (let ([xml_hash (make-hash)])
@@ -38,14 +38,19 @@
           (if (char=? ch #\<)
               (loop (read-char) defs  'READING_OR_KEY_END' keys values)
               (loop (read-char) defs status keys values))]
-         [(eq? status 'READING_OR_KEY_END')
+         [(eq? status 'READING_OR_KEY_END)
           (if (char=? ch #\>)
-              (if (string=? value (car defs))
-                  (if (pair? (cdr defs))
-                      (loop (read-char) defs 'KEY_START (cons value keys) '())
-                      (loop (read-char) defs 'VALUE (cons value keys) '()))
-                  (loop (read-char) defs'KEY_START keys values))
+              (let ([value (list->string (reverse values))])
+                (if (string=? value (caar defs))
+                    (if (pair? (cdr defs))
+                        (loop (read-char) defs 'KEY_START (cons value keys) '())
+                        (loop (read-char) defs 'VALUE (cons value keys) '()))
+                    (loop (read-char) defs'KEY_START keys values)))
               (loop (read-char) defs status keys (cons ch values)))]
+         [(eq? status 'VALUE)
+          (if (char=? ch  #\<)
+              (hash-set! xml_hash (foldr (lambda (key1 key2) (string-append key1 "," key2)) "" keys) (list->string (reverse values)))
+              (loop (read-char) defs status keys values))]
          [else
           (void)])))
     xml_hash))
