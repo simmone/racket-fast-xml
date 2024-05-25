dd70c9fc755562ab714dbdcd0d0a773a81678196
Chen Xiao <chenxiao770117@gmail.com>
2024-05-25 10:08:25 +0800

    update

--- fast-xml/main.rkt
+++ fast-xml/main.rkt
@@ -11,25 +11,41 @@
     (lambda ()
       (xml-port-to-hash (current-input-port) def))))
 
+; loop parameters:
+; ch: read a char each loop.
+; defs: searching defines, '(key1 (key2 (key3 . 'v)))
+; status: current search status.
+; keys: combine find keys to a list: '(key1 key2 key3...)
+; value: combine a string from chars
+
 ; 'START: scan start
-; 'WAITING_KEY_START: waiting to encounter '<'
-; 'READING_KEY: next chars will be read as key
-; 'KEY_END: next chars will be read as key
+; 'KEY_START: waiting to encounter '<'
+; 'READING_OR_KEY_END: next chars will be read as key, or waiting to encounter '>'
+; 'VALUE_OR_KEY_START: reading value
 
 (define (xml-port-to-hash xml_port def_pairs)
   (let ([xml_hash (make-hash)])
     (let loop ([ch (read-char xml_port)]
                [defs def_pairs]
                [status 'START]
-               [value ''])
+               [keys '()]
+               [values '()])
       (when (not (eof-object? ch))
         (cond
          [(eq? status 'START)
-          (loop ch 'WAITING_KEY value)]
-         [(eq? status 'WAITING_KEY)
+          (loop ch defs 'KEY_START keys values)]
+         [(eq? status 'KEY_START)
           (if (char=? ch #\<)
-              (loop (read-char) 'READING_KEY value)
-              (loop (read-char) 'WAITING_KEY value))]
+              (loop (read-char) defs  'READING_OR_KEY_END' keys values)
+              (loop (read-char) defs status keys values))]
+         [(eq? status 'READING_OR_KEY_END')
+          (if (char=? ch #\>)
+              (if (string=? value (car defs))
+                  (if (pair? (cdr defs))
+                      (loop (read-char) defs 'KEY_START (cons value keys) '())
+                      (loop (read-char) defs 'VALUE (cons value keys) '()))
+                  (loop (read-char) defs'KEY_START keys values))
+              (loop (read-char) defs status keys (cons ch values)))]
          [else
           (void)])))
     xml_hash))
