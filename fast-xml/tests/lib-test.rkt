00311c5f1e26f65200f17c4b64226d939ae232dc
Chen Xiao <chenxiao770117@gmail.com>
2024-05-29 19:55:31 +0800

    update

--- fast-xml/tests/lib-test.rkt
+++ fast-xml/tests/lib-test.rkt
@@ -9,16 +9,45 @@
    "test-lib"
 
    (test-case
-    "test-defs-to-list"
+    "test-defs-to-hash"
 
     (let ([defs '("topic.v")])
-      (check-equal? (defs-to-list defs) '(("topic" . "v")))
+      (check-equal? (defs-to-hash defs)
+                    '#hash(
+                           ("topic" . k)
+                           ("topic.v" . v)
+                           ))
 
     (let ([defs '("h1.h2.h3.topic")])
-      (check-equal? (defs-to-list defs) '(("h1" . ("h2" . ("h3" . "topic"))))))
+      (check-equal? (defs-to-hash defs)
+                    '#hash(
+                           ("h1" . k)
+                           ("h1.h2" . k)
+                           ("h1.h2.h3" . k)
+                           ("h1.h2.h3.topic" . v))))
 
     (let ([defs '("h1.h2.h3.topic" "h3.h4.h5.v")])
-      (check-equal? (defs-to-list defs) '(("h1" . ("h2" . ("h3" . "topic"))) ("h3" . ("h4" . ("h5" . "v"))))))
+      (check-equal? (defs-to-hash defs)
+                    '#hash(
+                           ("h1" . k)
+                           ("h1.h2" . k)
+                           ("h1.h2.h3" . k)
+                           ("h1.h2.h3.topic" . v)
+                           ("h3" . k)
+                           ("h3.h4" . k)
+                           ("h3.h4.h5" . k)
+                           ("h3.h4.h5.v" . v))))
+
+    (let ([defs '("h1.h2.h3.topic" "h1.h2.h4.topic")])
+      (check-equal? (defs-to-hash defs)
+                    '#hash(
+                           ("h1" . k)
+                           ("h1.h2" . k)
+                           ("h1.h2.h3" . k)
+                           ("h1.h2.h4" . k)
+                           ("h1.h2.h3.topic" . v)
+                           ("h1.h2.h4.topic" . v))))
+
       ))
   ))
 
