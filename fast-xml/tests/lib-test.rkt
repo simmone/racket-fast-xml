219839dc358757cec7f9a05248db9662e8129455
Chen Xiao <chenxiao770117@gmail.com>
2024-06-06 20:50:49 +0800

    update

--- fast-xml/tests/lib-test.rkt
+++ fast-xml/tests/lib-test.rkt
@@ -11,14 +11,14 @@
    (test-case
     "test-defs-to-hash"
 
-    (let ([def_hash (defs-to-hash '("topic.v"))])
+    (let ([def_hash (defs-to-hash '(("topic.v" . v)))])
       (check-equal? (hash-count def_hash) 2)
 
       (check-eq? (hash-ref def_hash "topic") 'k)
       (check-eq? (hash-ref def_hash "topic.v") 'v)
       )
 
-    (let ([def_hash (defs-to-hash '("h1.h2.h3.topic"))])
+    (let ([def_hash (defs-to-hash '(("h1.h2.h3.topic" . v)))])
       (check-equal? (hash-count def_hash) 4)
 
       (check-eq? (hash-ref def_hash "h1") 'k)
@@ -26,7 +26,8 @@
       (check-eq? (hash-ref def_hash "h1.h2.h3") 'k)
       (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'v))
 
-    (let ([def_hash (defs-to-hash '("h1.h2.h3.topic" "h3.h4.h5.v"))])
+    (let ([def_hash (defs-to-hash
+                      '(("h1.h2.h3.topic" . v) ("h3.h4.h5.v" . v))))])
       (check-equal? (hash-count def_hash) 8)
 
       (check-eq? (hash-ref def_hash "h1") 'k)
@@ -39,7 +40,7 @@
       (check-eq? (hash-ref def_hash "h3.h4.h5") 'k)
       (check-eq? (hash-ref def_hash "h3.h4.h5.v") 'v))
 
-    (let ([def_hash (defs-to-hash '("h1.h2.h3.topic" "h1.h2.h4.topic"))])
+    (let ([def_hash (defs-to-hash '(("h1.h2.h3.topic" . v) ("h1.h2.h4.topic" . a)))])
       (check-equal? (hash-count def_hash) 6)
 
       (check-eq? (hash-ref def_hash "h1") 'k)
@@ -47,23 +48,23 @@
       (check-eq? (hash-ref def_hash "h1.h2.h3") 'k)
       (check-eq? (hash-ref def_hash "h1.h2.h4") 'k)
       (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'v)
-      (check-eq? (hash-ref def_hash "h1.h2.h4.topic") 'v))
+      (check-eq? (hash-ref def_hash "h1.h2.h4.topic") 'a))
 
-    (let ([def_hash (defs-to-hash '("h1.h2.h3.topic" "h1.h2"))])
+    (let ([def_hash (defs-to-hash '(("h1.h2.h3.topic" . a) ("h1.h2" . v)))])
       (check-equal? (hash-count def_hash) 4)
 
       (check-eq? (hash-ref def_hash "h1") 'k)
       (check-eq? (hash-ref def_hash "h1.h2") 'v)
       (check-eq? (hash-ref def_hash "h1.h2.h3") 'k)
-      (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'v))
+      (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'a))
 
-    (let ([def_hash (defs-to-hash '("h1.h2" "h1.h2.h3.topic"))])
+    (let ([def_hash (defs-to-hash '(("h1.h2" . v) ("h1.h2.h3.topic" . a)))])
       (check-equal? (hash-count def_hash) 4)
 
       (check-eq? (hash-ref def_hash "h1") 'k)
       (check-eq? (hash-ref def_hash "h1.h2") 'v)
       (check-eq? (hash-ref def_hash "h1.h2.h3") 'k)
-      (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'v))
+      (check-eq? (hash-ref def_hash "h1.h2.h3.topic") 'a))
     )
   ))
 
