3d3fb222eb2d96b9a3322701c107b3648f22f10f
Chen Xiao <chenxiao770117@gmail.com>
2024-06-29 11:56:50 +0800

    update

--- fast-xml/tests/list/sheet-test.rkt
+++ fast-xml/tests/list/sheet-test.rkt
@@ -18,106 +18,180 @@
     (let ([xml_hash (xml-file-to-hash
                      sheet_xml_file
                      '(
-                       ("worksheet.xmlns" . a)
-                       ("worksheet.xmlns:r" . a)
-                       ("worksheet.dimension.ref" . a)
-                       ("worksheet.sheetViews.sheetView.selection" . v)
-                       ("worksheet.sheetViews.sheetView.selection.pane" . a)
-                       ("worksheet.sheetViews.sheetView.workbookViewId" . a)
-                       ("worksheet.sheetViews.sheetView.pane" . v)
-                       ("worksheet.sheetViews.sheetView.pane.ySplit" . a)
-                       ("worksheet.sheetViews.sheetView.pane.xSplit" . a)
-                       ("worksheet.sheetViews.sheetView.pane.topLeftCell" . a)
-                       ("worksheet.sheetViews.sheetView.pane.activePane" . a)
-                       ("worksheet.sheetViews.sheetView.pane.state" . a)
-                       ("worksheet.sheetFormatPr.defaultRowHeight" . a)
-                       ("worksheet.cols.col.min" . a)
-                       ("worksheet.cols.col.max" . a)
-                       ("worksheet.cols.col.width" . a)
-                       ("worksheet.sheetData.row.r" . a)
-                       ("worksheet.sheetData.row.spans" . a)
-                       ("worksheet.sheetData.row.c.r" . a)
-                       ("worksheet.sheetData.row.c.t" . a)
-                       ("worksheet.sheetData.row.c.v" . v)
-                       ("worksheet.phoneticPr" . v)
-                       ("worksheet.phoneticPr.fontId" . a)
-                       ("worksheet.phoneticPr.type" . a)
-                       ("worksheet.pageMargins" . v)
-                       ("worksheet.pageMargins.left" . a)
-                       ("worksheet.pageMargins.right" . a)
-                       ("worksheet.pageMargins.top" . a)
-                       ("worksheet.pageMargins.bottom" . a)
-                       ("worksheet.pageMargins.header" . a)
-                       ("worksheet.pageMargins.footer" . a)
-                       ("worksheet.pageSetup" . v)
-                       ("worksheet.pageSetup.paperSize" . a)
-                       ("worksheet.pageSetup.orientation" . a)
-                       ("worksheet.pageSetup.horizontalDpi" . a)
-                       ("worksheet.pageSetup.verticalDpi" . a)
-                       ("worksheet.pageSetup.r:id" . a)
+                       "worksheet.xmlns"
+                       "worksheet.xmlns:r"
+                       "worksheet.dimension.ref"
+                       "worksheet.sheetViews.sheetView.selection"
+                       "worksheet.sheetViews.sheetView.selection.pane"
+                       "worksheet.sheetViews.sheetView.workbookViewId"
+                       "worksheet.sheetViews.sheetView.pane"
+                       "worksheet.sheetViews.sheetView.pane.ySplit"
+                       "worksheet.sheetViews.sheetView.pane.xSplit"
+                       "worksheet.sheetViews.sheetView.pane.topLeftCell"
+                       "worksheet.sheetViews.sheetView.pane.activePane"
+                       "worksheet.sheetViews.sheetView.pane.state"
+                       "worksheet.sheetFormatPr.defaultRowHeight"
+                       "worksheet.cols.col.min"
+                       "worksheet.cols.col.max"
+                       "worksheet.cols.col.width"
+                       "worksheet.sheetData.row.r"
+                       "worksheet.sheetData.row.spans"
+                       "worksheet.sheetData.row.c.r"
+                       "worksheet.sheetData.row.c.t"
+                       "worksheet.sheetData.row.c.v"
+                       "worksheet.phoneticPr"
+                       "worksheet.phoneticPr.fontId"
+                       "worksheet.phoneticPr.type"
+                       "worksheet.pageMargins"
+                       "worksheet.pageMargins.left"
+                       "worksheet.pageMargins.right"
+                       "worksheet.pageMargins.top"
+                       "worksheet.pageMargins.bottom"
+                       "worksheet.pageMargins.header"
+                       "worksheet.pageMargins.footer"
+                       "worksheet.pageSetup"
+                       "worksheet.pageSetup.paperSize"
+                       "worksheet.pageSetup.orientation"
+                       "worksheet.pageSetup.horizontalDpi"
+                       "worksheet.pageSetup.verticalDpi"
+                       "worksheet.pageSetup.r:id"
                        ))])
 
-      (check-equal? (hash-ref xml_hash "worksheet.xmlns") '("http://schemas.openxmlformats.org/spreadsheetml/2006/main"))
-      (check-equal? (hash-ref xml_hash "worksheet.xmlns:r") '("http://schemas.openxmlformats.org/officeDocument/2006/relationships"))
+      (check-equal? (hash-ref xml_hash "worksheet1.xmlns1") "http://schemas.openxmlformats.org/spreadsheetml/2006/main")
+      (check-equal? (hash-ref xml_hash "worksheet1.xmlns:r1") "http://schemas.openxmlformats.org/officeDocument/2006/relationships")
 
-      (check-equal? (hash-ref xml_hash "worksheet.dimension.ref") '("A1:F4"))
+      (check-equal? (hash-ref xml_hash "worksheet1.dimension1.ref1") "A1:F4")
 
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.selection") '("" "" ""))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.selection.pane") '("bottomLeft" "topRight" "bottomRight"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection1") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection2") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection3") "")
 
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.workbookViewId") '("0"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane") '(""))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane.ySplit") '("1"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane.xSplit") '("1"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane.topLeftCell") '("B2"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane.activePane") '("bottomRight"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetViews.sheetView.pane.state") '("frozen"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection1.pane1") "bottomLeft")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection2.pane1") "topRight")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.selection3.pane1") "bottomRight")
 
-      (check-equal? (hash-ref xml_hash "worksheet.sheetFormatPr.defaultRowHeight") '("13.5"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.workbookViewId1") "0")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1.ySplit1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1.xSplit1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1.topLeftCell1") "B2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1.activePane1") "bottomRight")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetViews1.sheetView1.pane1.state1") "frozen")
 
-      (check-equal? (hash-ref xml_hash "worksheet.cols.col.min") '("1" "3" "5" "6"))
-      (check-equal? (hash-ref xml_hash "worksheet.cols.col.max") '("2" "4" "5" "6"))
-      (check-equal? (hash-ref xml_hash "worksheet.cols.col.width") '("50" "8" "14" "8"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetFormatPr1.defaultRowHeight1") "13.5")
 
-      (check-equal? (hash-ref xml_hash "worksheet.sheetData.row.r") '("1" "2" "3" "4"))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetData.row.spans") '("1:6" "1:6" "1:6" "1:6"))
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col1.min1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col2.min1") "3")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col3.min1") "5")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col4.min1") "6")
 
-      (check-equal? (hash-ref xml_hash "worksheet.sheetData.row.c.r") '("A1" "B1" "C1" "D1" "E1" "F1"
-                                                                        "A2" "B2" "C2" "D2" "E2" "F2"
-                                                                        "A3" "B3" "C3" "D3" "E3" "F3"
-                                                                        "A4" "B4" "C4" "D4" "E4" "F4"
-                                                                        ))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetData.row.c.t") '(
-                                                                        "s" "s" "s" "s" "s" "s"
-                                                                        "s" ""  ""  ""  ""  ""
-                                                                        "s" ""  ""  ""  ""  ""
-                                                                        "s" ""  ""  ""  ""  ""
-                                                                        ))
-      (check-equal? (hash-ref xml_hash "worksheet.sheetData.row.c.v") '(
-                                                                        "16" "1" "2" "3" "4" "5"
-                                                                        "8" "100" "300" "200" "0.6934" "43360"
-                                                                        "13" "200" "400" "300" "139999.89223" "43361"
-                                                                        "6" "300" "500" "400" "23.34" "43362"
-                                                                        ))
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col1.max1") "2")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col2.max1") "4")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col3.max1") "5")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col4.max1") "6")
 
-      (check-equal? (hash-ref xml_hash "worksheet.phoneticPr") '(""))
-      (check-equal? (hash-ref xml_hash "worksheet.phoneticPr.fontId") '("1"))
-      (check-equal? (hash-ref xml_hash "worksheet.phoneticPr.type") '("noConversion"))
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col1.width1") "50")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col2.width1") "8")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col3.width1") "14")
+      (check-equal? (hash-ref xml_hash "worksheet1.cols1.col4.width1") "8")
 
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins") '(""))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.left") '("0.7"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.right") '("0.7"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.top") '("0.75"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.bottom") '("0.75"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.header") '("0.3"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageMargins.footer") '("0.3"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.r1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.r1") "2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.r1") "3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.r1") "4")
 
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup") '(""))
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup.paperSize") '("9"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup.orientation") '("portrait"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup.horizontalDpi") '("200"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup.verticalDpi") '("200"))
-      (check-equal? (hash-ref xml_hash "worksheet.pageSetup.r:id") '("rId1"))
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.spans1") "1:6")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.spans1") "1:6")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.spans1") "1:6")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.spans1") "1:6")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c1.r1") "A1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c2.r1") "B1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c3.r1") "C1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c4.r1") "D1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c5.r1") "E1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c6.r1") "F1")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c1.r1") "A2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c2.r1") "B2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c3.r1") "C2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c4.r1") "D2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c5.r1") "E2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c6.r1") "F2")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c1.r1") "A3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c2.r1") "B3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c3.r1") "C3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c4.r1") "D3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c5.r1") "E3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c6.r1") "F3")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c1.r1") "A4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c2.r1") "B4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c3.r1") "C4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c4.r1") "D4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c5.r1") "E4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c6.r1") "F4")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c1.t1") "s")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c2.t1") "s")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c3.t1") "s")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c4.t1") "s")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c5.t1") "s")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c6.t1") "s")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c1.t1") "s")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c1.t1") "s")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c1.t1") "s")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c1.v1") "16")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c2.v1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c3.v1") "2")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c4.v1") "3")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c5.v1") "4")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row1.c6.v1") "5")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c1.v1") "8")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c2.v1") "100")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c3.v1") "300")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c4.v1") "200")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c5.v1") "0.6934")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row2.c6.v1") "43360")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c1.v1") "13")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c2.v1") "200")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c3.v1") "400")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c4.v1") "300")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c5.v1") "139999.89223")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row3.c6.v1") "43361")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c1.v1") "6")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c2.v1") "300")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c3.v1") "500")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c4.v1") "400")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c5.v1") "23.34")
+      (check-equal? (hash-ref xml_hash "worksheet1.sheetData1.row4.c6.v1") "43362")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.phoneticPr1") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.phoneticPr1.fontId1") "1")
+      (check-equal? (hash-ref xml_hash "worksheet1.phoneticPr1.type1") "noConversion")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.left1") "0.7")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.right1") "0.7")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.top1") "0.75")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.bottom1") "0.75")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.header1") "0.3")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageMargins1.footer1") "0.3")
+
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1") "")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1.paperSize1") "9")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1.orientation1") "portrait")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1.horizontalDpi1") "200")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1.verticalDpi1") "200")
+      (check-equal? (hash-ref xml_hash "worksheet1.pageSetup1.r:id1") "rId1")
       ))
 
    (test-case
